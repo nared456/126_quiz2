b8d4cad9e581f08aada9ad41bb823be405140f3d
nared456 <78350512+nared456@users.noreply.github.com>
2021-03-25 17:19:35 +0700

    6320503041

---
+++ Cube_free_number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int N, i = 2, ch = 0, j=1;
+    scanf("%d", &N);
+    int lo = 0;
+    while (i <= 1000000)
+    {
+        if (N % (i * i * i) == 0)
+        {
+            printf("Not Cube Free");
+            ch++;
+            break;
+        }
+        i++;
+    }
+    if (ch != 1)
+    {
+        int k = 2;
+        while (j <= N)
+        {
+            if (j % (k * k * k))
+            {
+                lo -= 1;
+            }
+            else
+            {
+                lo +=1;
+            }
+            k++;
+            j++;
+        }
+        printf("%d",lo);
+    }
+}
